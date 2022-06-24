721c2a9467fa596c3c79ada0bfa7017d93d35e63
Syed Wajahat Ali <wajahat.klk25.com>
2022-06-24 18:52:01 +0200

    IT 3 error on routes

--- routes/drones.js
+++ routes/drones.js
@@ -1,29 +1,34 @@
 const express = require('express');
+const { connection } = require('mongoose');
 const router = express.Router();
 const Drone = require('../models/Drone.model');
 // require the Drone model here
 
-router.get('/drones', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   // Iteration #2: List the drones
   try {
     const drones = await Drone.find({});
     res.render('drones/list', {drones:drones});
-    console.log(drones)
   } catch (error) {
     console.log(error)
     next(error)
-  };
+  }
   
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+   
+  Drone.create(req.body)
+  .then (()=>{
+    res.redirect('/drones');
+  })
+  .catch (error => console.log('Drone was"nt created try again!', error))
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
